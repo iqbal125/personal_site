8f88f69b46688debaf57e4d35224cc37663506e9
iqbal125 <iqbal125@yahoo.com>
2020-07-23 19:13:43 -0700

    added landing section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,19 @@
 import React from "react"
 import styles from "./home.module.css"
 import image1 from "../../static/illustrations/undraw_web_developer_p3e5.svg"
+import image2 from "../../static/illustrations/undraw_code_review_l1q9.svg"
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1> Main Title</h1>
-      <img className={styles.hero_image} src={image1} alt="" />
+    <div>
+      <div className={styles.hero_container}>
+        <h1 className={styles.hero_text}> Main Title</h1>
+        <img className={styles.hero_image} src={image1} alt="" />
+      </div>
+      <div className={styles.row1}>
+        <img className={styles.row1_image} src={image2} alt="" />
+        <h1>Row 1</h1>
+      </div>
     </div>
   )
 }
