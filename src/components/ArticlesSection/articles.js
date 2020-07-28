d4e06d211e84e0708894d235f4998a690d222fd9
iqbal125 <iqbal125@yahoo.com>
2020-07-27 17:37:11 -0700

    added ur to articles

--- src/components/ArticlesSection/articles.js
+++ src/components/ArticlesSection/articles.js
@@ -4,53 +4,69 @@ const articles = [
       "Full Stack React: How to Build Your Own Blog Using Express, Hooks, & Postgres.",
     date: "3 OCTOBER 2019",
     ranking: `#1 for "React Fullstack Blog"`,
+    url:
+      "https://www.freecodecamp.org/news/fullstack-react-blog-app-with-express-and-psql/",
   },
   {
     title:
       "How to build a React Hooks front-end app with routing and authentication",
     date: "30 SEPTEMBER 2019",
     ranking: `#2 for "React Hooks Routing and Authentication"`,
+    url:
+      "https://www.freecodecamp.org/news/build-a-react-hooks-front-end-app-with-routing-and-authentication/",
   },
   {
     title:
       "How to Build a Real-time React and Express Fullstack search engine with Postgres",
     date: "16 SEPTEMBER 2019",
     ranking: `#1 for "React Postgres Search Engine"`,
+    url:
+      "https://www.freecodecamp.org/news/react-express-fullstack-search-engine-with-psql/",
   },
   {
     title:
       "How to deploy a React app to production on AWS using Express, Postgres, PM2 and nginx",
     date: "11 SEPTEMBER 2019",
     ranking: `#5 "React AWS Deployment"`,
+    url:
+      "https://www.freecodecamp.org/news/production-fullstack-react-express/",
   },
   {
     title:
       "How to deploy a React + Node app to Heroku in 3 minutes without the command line",
     date: "7 SEPTEMBER 2019",
     ranking: `#3 for "React Heroku Deployment"`,
+    url: "https://www.freecodecamp.org/news/deploy-a-react-node-app-to/",
   },
   {
     title:
       "How to set up performance and user tracking in React with Google Analytics",
     date: "31 JULY 2019",
     ranking: `#5 for "React Performance Tracking"`,
+    url:
+      "https://www.freecodecamp.org/news/performance-and-user-tracking-in-react-with-google-analytics/",
   },
   {
     title: "How to Test React Components: the Complete Guide",
     date: "11 JULY 2019",
     ranking: `#5 for "React Testing Tutorial"`,
+    url: "https://www.freecodecamp.org/news/testing-react-hooks/",
   },
   {
     title:
       "How to convert from React-Redux classes to React Hooks, the easy way",
     date: "15 MAY 2019",
     ranking: `#3 for "Convert Redux to React Hooks"`,
+    url:
+      "https://www.freecodecamp.org/news/how-to-convert-from-react-redux-classes-to-react-hooks-the-easy-way-eca2233e0e7a/",
   },
   {
     title:
       "How to integrate React Hooks into your project without changing your Redux code",
     date: "22 MARCH 2019",
     ranking: `#6 for "Integrate React Hooks into Redux"`,
+    url:
+      "https://www.freecodecamp.org/news/how-to-integrate-react-hooks-into-your-project-without-changing-your-redux-code-974e6f70f0b0/",
   },
 ]
 
--- src/components/ArticlesSection/index.js
+++ src/components/ArticlesSection/index.js
@@ -4,18 +4,34 @@ import styles from "./articlessection.module.css"
 
 const ArticlesSection = () => (
   <div>
+    <div className={styles.text_section}>
+      <h2>
+        My Coding Tutorials have Provided exceptional value to fellow engineers
+        and have consequently reached extremely high Google search rankings
+      </h2>
+      <p>
+        All my tutorials are on the First page of Google for their respective
+        search terms and a few are the top search result, Current as of 7/27/20
+      </p>
+      <p>
+        Get in touch with me to see how I can help you reach high Google Search
+        rankings
+      </p>
+    </div>
     <div className={styles.card_row}>
       {articles.map(article => (
         <div className={styles.card}>
-          <div className={styles.header}>
-            <h3>{article.title}</h3>
-          </div>
-          <small>{article.date}</small>
+          <a href={article.url} rel="noopener noreferrer" target="_blank">
+            <div className={styles.header}>
+              <h3>{article.title}</h3>
+            </div>
+            <small>{article.date}</small>
 
-          <p>
-            <strong>Google Search Ranking:</strong>
-          </p>
-          <div> {article.ranking}</div>
+            <p>
+              <strong>Google Search Ranking:</strong>
+            </p>
+            <div> {article.ranking}</div>
+          </a>
         </div>
       ))}
     </div>
