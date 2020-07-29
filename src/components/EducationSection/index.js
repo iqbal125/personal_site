332d4b6db361601d7a994109a8173d09c9435316
iqbal125 <iqbal125@yahoo.com>
2020-07-28 17:20:15 -0700

    added education section

---
+++ src/components/EducationSection/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const EducationSection = () => <div></div>
+
+export default EducationSection
---
+++ src/pages/education.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import EducationSection from "../components/ArticlesSection"
+
+const Education = () => (
+  <Layout>
+    <EducationSection />
+  </Layout>
+)
+
+export default Education
