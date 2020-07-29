306de3e3d465c9a50f7f75c361280c3f58e81309
iqbal125 <iqbal125@yahoo.com>
2020-07-28 19:10:14 -0700

    added education section

--- src/components/EducationSection/education.js
+++ src/components/EducationSection/education.js
@@ -1,3 +1,56 @@
-export const degrees = [{ degree: "", school: "", description: "", img: "" }]
+export const degrees = [
+  {
+    degree: "BS Business Adminstration",
+    school: "San Francisco State University",
+    description:
+      "A comprehensive business education covering everything from marketing to operations",
+    learned: `consumer behavior, business operations, digital marketing, 
+    accounting, web design, business budgeting`,
+  },
+  {
+    degree: "MS Data Analytics",
+    school: "Western Govenors University",
+    description: "Advanced Technical Degree, math and coding heavy program",
+    learned:
+      "SQL, Python, SAS, R. Coded several Machine Learning Models as part of Program",
+  },
+]
 
-export const certs = [{ title: "", description: "", img: "" }]
+export const certs = [
+  {
+    title: "AWS Certified Solution Architect- Associate",
+    description: "",
+    img: "",
+    url: "",
+  },
+  {
+    title: "SAS Programmer II - Business Analyst",
+    description: "Validates my ability to build machine learning models in SAS",
+    img: "",
+  },
+  {
+    title: "Udacity Fullstack Nanodegree",
+    description: "Completed 5 fullstack projects as part of program",
+    img: "",
+  },
+  {
+    title: "Udacity Deep Learning Nanodegree",
+    description: "Completed 4 deep learning models as part of program",
+    img: "",
+  },
+  {
+    title: "Udacity Front End Nanodegree",
+    description: "Completed 6 front end projects as part of program",
+    img: "",
+  },
+  {
+    title: "Udacity Data Analyst Nanodegree",
+    description: "Completed 4 data analysis projects as part of degree",
+    img: "",
+  },
+  {
+    title: "Udacity Intro to Programming Nanodegree",
+    description: "Completed 3 basic programming projects",
+    img: "",
+  },
+]
--- src/components/EducationSection/index.js
+++ src/components/EducationSection/index.js
@@ -1,8 +1,32 @@
 import React from "react"
+import image1 from "../../static/Education/1581585555.svg"
+import styles from "./education.module.css"
+import { degrees, certs } from "./education"
 
 const EducationSection = () => (
-  <div>
-    <h1>EducationSection</h1>
+  <div className={styles.top_row}>
+    <div className={styles.college_header}>
+      <img className={styles.icon} src={image1} alt="" />
+      <h2>College Degrees</h2>
+    </div>
+    <div>
+      {degrees.map(degree => (
+        <div>
+          <div>{degree.degree}</div>
+          <div>{degree.school}</div>
+          <div>{degree.description}</div>
+          <div>{degree.learned}</div>
+        </div>
+      ))}
+    </div>
+    <div>
+      {certs.map(cert => (
+        <div>
+          <div>{cert.title}</div>
+          <div>{cert.description}</div>
+        </div>
+      ))}
+    </div>
   </div>
 )
 
