a908d3620ad070ae24ef3529d2a04ab4017dcf3f
iqbal125 <iqbal125@yahoo.com>
2020-07-30 17:33:18 -0700

    added more project descriptions

--- src/components/ProjectsSection/projects.js
+++ src/components/ProjectsSection/projects.js
@@ -1,4 +1,34 @@
 const projects = [
-  { title: "", description: "", img: "", testimonial: "" },
-  { title: "", description: "", img: "", testimonial: "" },
+  {
+    title: "jobtrackr",
+    description:
+      "An Application that organizes and tracks all your job applications in one place",
+    url: "",
+    img: "",
+    technologies: "",
+    testimonial: "",
+  },
+  {
+    title: "Server-side Auth",
+    description:
+      "Added an Auth0 and Node Authentication Server to an existing project",
+    img: "",
+    technologies: "",
+    testimonial: "",
+  },
+  {
+    title: "Postgres Server Side Auth",
+    description:
+      "A full complex Authentication Server built from the ground up",
+    img: "",
+    technologies: "",
+    testimonial: "",
+  },
+  {
+    title: "Concord Site",
+    description: "Built web dev agency site from a blank page",
+    img: "",
+    technologies: "",
+    testimonial: "",
+  },
 ]
