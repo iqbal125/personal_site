2dab2fcd3e3d0e83efaea54907baf8659ae29f7f
iqbal125 <iqbal125@yahoo.com>
2020-08-13 18:14:24 -0700

    made header responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,13 +28,92 @@ const Header = ({ props }) => {
               <div onClick={navLinksHandler} className={styles.close_button}>
                 <AiOutlineClose />
               </div>
+            )}{" "}
+            {/* Mobile Hamburger Links*/}
+            {navLinks && (
+              <>
+                <div className={styles.dropdown}>
+                  <Link
+                    to="/projects"
+                    className={styles.header_links_mobile}
+                    activeClassName={styles.header_link_active}
+                  >
+                    Projects
+                  </Link>
+                  <Link
+                    to="/courses"
+                    className={styles.header_links_mobile}
+                    activeClassName={styles.header_link_active}
+                  >
+                    Courses
+                  </Link>
+                  <Link
+                    to="/opensource"
+                    className={styles.header_links_mobile}
+                    activeClassName={styles.header_link_active}
+                  >
+                    Open-Source
+                  </Link>
+                  <Link
+                    to="/education"
+                    className={styles.header_links_mobile}
+                    activeClassName={styles.header_link_active}
+                  >
+                    Education
+                  </Link>
+                  <Link
+                    to="/articles"
+                    className={styles.header_links_mobile}
+                    activeClassName={styles.header_link_active}
+                  >
+                    Articles
+                  </Link>
+                </div>
+              </>
             )}
           </div>
         </div>
 
         <div className={styles.mid_header}>
           {/* Desktop */}
-          <div>Mid Header</div>
+          <div className={styles.desktop_links}>
+            <Link
+              to="/projects"
+              className={styles.header_links_mobile}
+              activeClassName={styles.header_link_active}
+            >
+              Projects
+            </Link>
+            <Link
+              to="/courses"
+              className={styles.header_links_mobile}
+              activeClassName={styles.header_link_active}
+            >
+              Courses
+            </Link>
+            <Link
+              to="/opensource"
+              className={styles.header_links_mobile}
+              activeClassName={styles.header_link_active}
+            >
+              Open-Source
+            </Link>
+
+            <Link
+              to="/education"
+              className={styles.header_links_mobile}
+              activeClassName={styles.header_link_active}
+            >
+              Education
+            </Link>
+            <Link
+              to="/articles"
+              className={styles.header_links_mobile}
+              activeClassName={styles.header_link_active}
+            >
+              Articles
+            </Link>
+          </div>
           {/* Mobile */}
           <div className={styles.mobile_logo}>
             <Link to="/">Logo</Link>
@@ -42,90 +121,12 @@ const Header = ({ props }) => {
         </div>
 
         <div className={styles.right_header}>
-          {/* Desktop */}
-          <div className={styles.desktop_links}>
-            <Link
-              to="/opensource"
-              className={styles.header_link}
-              activeClassName={styles.header_link_active}
-            >
-              Open-Source
-            </Link>
-            <Link
-              to="/articles"
-              className={styles.header_link}
-              activeClassName={styles.header_link_active}
-            >
-              Articles
-            </Link>
-            <Link
-              to="/courses"
-              className={styles.header_link}
-              activeClassName={styles.header_link_active}
-            >
-              Courses
-            </Link>
-            <Link
-              to="/education"
-              className={styles.header_link}
-              activeClassName={styles.header_link_active}
-            >
-              Education
-            </Link>
-            <Link
-              to="/projects"
-              className={styles.header_link}
-              activeClassName={styles.header_link_active}
-            >
-              Projects
-            </Link>
-          </div>
+          {/* Mobile */}
+          <Link to="/" className={styles.contact_button}>
+            Contact
+          </Link>
         </div>
-        {/* Mobile */}
-        <div className={styles.mobile_search}></div>
       </header>
-      {/* Mobile Hamburger Links*/}
-      {navLinks && (
-        <>
-          <div className={styles.dropdown_home}>
-            <Link
-              to="/opensource"
-              className={styles.header_links_mobile}
-              activeClassName={styles.header_link_active}
-            >
-              Open-Source
-            </Link>
-            <Link
-              to="/opensource"
-              className={styles.header_links_mobile}
-              activeClassName={styles.header_link_active}
-            >
-              Articles
-            </Link>
-            <Link
-              to="/courses"
-              className={styles.header_links_mobile}
-              activeClassName={styles.header_link_active}
-            >
-              Courses
-            </Link>
-            <Link
-              to="/education"
-              className={styles.header_links_mobile}
-              activeClassName={styles.header_link_active}
-            >
-              Education
-            </Link>
-            <Link
-              to="/projects"
-              className={styles.header_links_mobile}
-              activeClassName={styles.header_link_active}
-            >
-              Projects
-            </Link>
-          </div>
-        </>
-      )}
     </>
   )
 }
