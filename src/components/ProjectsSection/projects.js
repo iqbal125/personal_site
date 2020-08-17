1debec1fc2997337f0d94f80da422bb85d7a2201
iqbal125 <iqbal125@yahoo.com>
2020-08-16 17:42:01 -0700

    added project descriptions

--- src/components/ProjectsSection/projects.js
+++ src/components/ProjectsSection/projects.js
@@ -1,25 +1,21 @@
 export const projects = [
   {
     title: "jobtrackr",
-    description:
-      "An Application that organizes and tracks all your job applications in one place",
+    description: `Worked on a team of 5 engineers and 1 PM on this project. We built a job application tracking app with React, Node, and Postgres. I helped code for all parts of the app including front end, server side and database. I built out the entire authentication system with Firebase Auth as well. This was a good learning experience, because I learned to work with the knex ORM which I didn't know previously. I also learned about web design and UX research from the PM.   `,
     img: "",
     view_code_url: ``,
     view_app_url: ``,
   },
   {
-    title: "Server-side Auth",
-    description:
-      "Added an Auth0 and Node Authentication Server to an existing project, Note: this is a Server Side only project",
+    title: "Auth0 Authentication",
+    description: `Added Auth0 Authentication in an existing internal app. I first integrated the authentication to the internal front end app, then integrated it to the node server. I also set up all the configuration on the Auth0 site. I worked very closely with the client in this one and was coding while with him on the phone a few times. I explained how Auth0 and JWT authentication works to the client in order to help him make the best decisions about what authentication solution to go with. `,
     img: "",
     view_code_url: ``,
     view_app_url: ``,
   },
   {
     title: "Postgres Server Side Auth",
-    description:
-      "A full complex Authentication Server built from the ground up",
-    details: `This was one of the most difficult projects Ive ever done. It was a full authentication server, built from the ground up. 
+    description: `This was one of the most difficult projects I've ever done. It was a full authentication server, built from the ground up. 
       What made it particularly hard was that there were literally no tutorials on the internet on how to do it. The especially tricky parts were the “password reset” and “password forget” features, but I was able to get it done through a lot of trial and error. 
       I also had to learn a lot of complex concepts like cryptography and hashing passwords. `,
     img: "",
@@ -28,28 +24,14 @@ export const projects = [
   },
   {
     title: "Concord Site",
-    description: "Built web dev agency site from a blank page",
+    description: `Built a web Dev agency site from scratch. I built this site entirely in Gatsby, a React static site generator. This was a fun project that let me explore the full capabilities of Gatsby and showed me how powerful and versatile it was, and not just a simple static site generator. I learned about popular modern styling in this app with illustrations and svg waves. The text truncation on the profile cards was also an elegant solution I was proud of on this project.`,
     img: "",
     view_code_url: ``,
     view_app_url: ``,
   },
   {
     title: "myRide",
-    description: "Built web dev agency site from a blank page",
-    img: "",
-    view_code_url: ``,
-    view_app_url: ``,
-  },
-  {
-    title: "Analyzing Customer Churn Using Python Machine Learning",
-    description: "Built web dev agency site from a blank page",
-    img: "",
-    view_code_url: ``,
-    view_app_url: ``,
-  },
-  {
-    title: "myRide",
-    description: "Built web dev agency site from a blank page",
+    description: `Currently working on this  self driving car platform project as a long time engineer. I’m mainly responsible for the internal app portal which lists out a list of coordinates and tasks for the cars. I mainly worked with React and extensively with Google maps on the frontend. I also worked with node and AWS DynamoDB on the server side. I also built a small demonstration app with React Native and Expo for fun.`,
     img: "",
     view_code_url: ``,
     view_app_url: ``,
