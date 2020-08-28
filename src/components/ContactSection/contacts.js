257b90b1db2389b1aab5853c9724cb38228f69cc
iqbal125 <iqbal125@yahoo.com>
2020-08-27 18:22:31 -0700

    added svg icons

---
+++ src/components/ContactSection/contacts.js
@@ -0,0 +1,13 @@
+import image2 from "../../static/OpenSource/github-ar21.svg"
+import image3 from "../../static/OpenSource/twitter-ar21.svg"
+import image4 from "../../static/OpenSource/udemy-ar21.svg"
+import image5 from "../../static/OpenSource/stackoverflow-ar21.svg"
+
+const contacts = [
+  { id: 2, icon: image2, url: "" },
+  { id: 3, icon: image3, url: "" },
+  { id: 4, icon: image4, url: "" },
+  { id: 5, icon: image5, url: "" },
+]
+
+export default contacts
--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,5 +1,20 @@
 import React from "react"
+import contacts from "./contacts"
+import styles from "./contact.module.css"
+import image1 from "../../static/OpenSource/linkedin-ar21.svg"
+import image6 from "../../static/OpenSource/freecodecamp.svg"
 
-const ContactSection = () => <div></div>
+const ContactSection = () => (
+  <div className={styles.container}>
+    <img src={image1} className={styles.linked} />
+    <div className={styles.contact_row}>
+      {contacts.map(contact => (
+        <img className={styles.icon} src={contact.icon} />
+      ))}
+    </div>
+
+    <img src={image6} className={styles.img2} />
+  </div>
+)
 
 export default ContactSection
