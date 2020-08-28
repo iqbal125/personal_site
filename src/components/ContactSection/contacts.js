e24299424a51cce191d6c8883c959cfbf7bbb114
iqbal125 <iqbal125@yahoo.com>
2020-08-27 19:01:36 -0700

    finish contact page

--- src/components/ContactSection/contacts.js
+++ src/components/ContactSection/contacts.js
@@ -2,12 +2,18 @@ import image2 from "../../static/OpenSource/github-ar21.svg"
 import image3 from "../../static/OpenSource/twitter-ar21.svg"
 import image4 from "../../static/OpenSource/udemy-ar21.svg"
 import image5 from "../../static/OpenSource/stackoverflow-ar21.svg"
+import image6 from "../../static/SkillsIcons/amazon_aws-ar21.svg"
 
 const contacts = [
-  { id: 2, icon: image2, url: "" },
-  { id: 3, icon: image3, url: "" },
-  { id: 4, icon: image4, url: "" },
-  { id: 5, icon: image5, url: "" },
+  { id: 2, icon: image2, url: "https://github.com/iqbal125" },
+  { id: 3, icon: image3, url: "https://twitter.com/iqbal125sf" },
+  { id: 4, icon: image4, url: "https://www.udemy.com/user/mohammad-iqbal-8/" },
+  {
+    id: 5,
+    icon: image5,
+    url: "https://stackoverflow.com/users/7355935/iqbal125",
+  },
+  { id: 6, icon: image6, url: "" },
 ]
 
 export default contacts
--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -7,17 +7,33 @@ import image6 from "../../static/OpenSource/freecodecamp.svg"
 const ContactSection = () => (
   <div className={styles.container}>
     <h2>Connect with me on LinkedIn for Business inquiries</h2>
-    <img src={image1} className={styles.linked} />
+    <a
+      href="https://www.linkedin.com/in/iqbal125/"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <img src={image1} className={styles.linked} />
+    </a>
     <h3>
       Feel Free to connect with me on my other accounts just to chat or to see
       my work
     </h3>
     <div className={styles.contact_row}>
       {contacts.map(contact => (
-        <img className={styles.icon} src={contact.icon} />
+        <div className={styles.icon_wrap}>
+          <a href={contact.url} rel="noopener noreferrer" target="_blank">
+            <img className={styles.icon} src={contact.icon} />
+          </a>
+        </div>
       ))}
     </div>
-    <img src={image6} className={styles.freecode} />
+    <a
+      href="https://www.freecodecamp.org/news/author/iqbal125/"
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <img src={image6} className={styles.freecode} />
+    </a>
   </div>
 )
 
