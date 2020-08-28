b0a8abe0db34340d34aaeddc0bb42a041ebf9ac2
iqbal125 <iqbal125@yahoo.com>
2020-08-27 18:32:19 -0700

    added styling

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -6,14 +6,18 @@ import image6 from "../../static/OpenSource/freecodecamp.svg"
 
 const ContactSection = () => (
   <div className={styles.container}>
+    <h2>Connect with me on LinkedIn for Business inquiries</h2>
     <img src={image1} className={styles.linked} />
+    <h3>
+      Feel Free to connect with me on my other accounts just to chat or to see
+      my work
+    </h3>
     <div className={styles.contact_row}>
       {contacts.map(contact => (
         <img className={styles.icon} src={contact.icon} />
       ))}
     </div>
-
-    <img src={image6} className={styles.img2} />
+    <img src={image6} className={styles.freecode} />
   </div>
 )
 
